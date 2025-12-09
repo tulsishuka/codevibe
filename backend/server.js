186250f5efdada4de555797d78681db1faada5e0
Tulasi <tulsishuklag@gmail.com>
2025-12-09 12:52:02 +0530

    setup server file for mongodb database

--- backend/server.js
+++ backend/server.js
@@ -2,42 +2,40 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cors = require('cors');
-
+const connectDB = require('./config/db');
+const User = require('./model/User');
 app.use(cors());
-
 app.use(express.json()); 
 
-users = []; 
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Express REST API');
 });
 
-// POST: Create new user
-app.post('/register', (req, res) => {
-  console.log("Received request body:", req.body);
-  const { username, email,password } = req.body;
-console.log("haga",req.body);
-  // Validate input data
-  if (!username || !email  || !password) {
-    return res.status(400).json({ message: 'Name and email Password are required' });
+
+
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: "Name, email, and password are required" });
   }
-console.log("haga1",req.body);
-  const newId = users.length > 0 ? Math.max(...users.map(user => user.id)) + 1 : 1;
-
   const newUser = {
-    id: newId,
     username,
-    email
+    email,
+    password
   };
-console.log("haga2",newUser);
-  users.push(newUser);
 
-  res.status(201).json({ message: 'User created successfully', newUser });
+  try {
+    const savedUser = await User.create(newUser);
+    res.status(201).json({ message: "User created successfully", newUser: savedUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
-
-// Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
