7ef8ad237a7ebd2da4573bc622ee66bba84f3360
Tulasi <tulsishuklag@gmail.com>
2025-12-09 12:53:53 +0530

    create page folder

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1 className="text-center font-bold text-lg">
+        Dashboard Page
+      </h1>
+      
+    </div>
+  )
+}
+
+export default Dashboard
