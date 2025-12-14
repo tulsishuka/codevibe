208fad50ec9db867b91dbe0fd456eac0ef28073c
Tulasi <tulsishuklag@gmail.com>
2025-12-14 16:01:51 +0530

    add login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-
+import Login from "./pages/Login";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Register />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
